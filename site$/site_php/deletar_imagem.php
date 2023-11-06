c188e3dde8e48e07222b95a02ca6cafa7f5a9d1f
Leonardo Cesar <127048383+Leo2828@users.noreply.github.com>
2023-11-06 08:48:28 -0300

    Update deletar_imagem.php

--- site$/site_php/deletar_imagem.php
+++ site$/site_php/deletar_imagem.php
@@ -3,9 +3,10 @@
     require "conexao.php";
 
     $id = $_GET["idImg"];
+    $idProduto = $_GET["idProduto"];
     
     $comando = "delete from Imagem where idImg=$id;";
     mysqli_query($conexao, $comando);
-    header('Location: '."../html/editar_produto.php")
+    header('Location: '."../html/editar_produto.php?idProduto=$idProduto");
 
 ?>
