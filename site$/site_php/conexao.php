653892ecb2480a271da6be81a21fd9423eec608c
Leonardo Cesar <127048383+Leo2828@users.noreply.github.com>
2023-11-06 08:05:45 -0300

    Update conexao.php

--- site$/site_php/conexao.php
+++ site$/site_php/conexao.php
@@ -1,5 +1,5 @@
 <?php
 
-    $conexao = mysqli_connect('if0_35365891_homers_confeitaria', 'if0_35365891', 'pato2828', 'sql109.infinityfree.com');
+    $conexao = mysqli_connect('localhost', 'root', '', 'homers_confeitaria');
 
 ?>
