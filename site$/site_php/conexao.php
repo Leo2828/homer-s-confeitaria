2fc4d625767c59f4581f775ac72ab3e37ee73750
Leonardo Cesar <127048383+Leo2828@users.noreply.github.com>
2023-11-09 22:13:25 -0300

    Update conexao.php

--- site$/site_php/conexao.php
+++ site$/site_php/conexao.php
@@ -1,5 +1,5 @@
 <?php
 
-    $conexao = mysqli_connect('localhost', 'root', 'Leoleo601@', 'homers_confeitaria');
+    $conexao = mysqli_connect('localhost', 'eusluvnc_homers_confeitaria', 'pato2828', 'eusluvnc_homers_confeitaria');
 
 ?>
